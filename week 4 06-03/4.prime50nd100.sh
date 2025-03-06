728fea3e5a48a8b1e04bb9964fba8a817e9cc0a6
naadhi-web <naadhifaisal@gmail.com>
2025-03-06 10:57:31 +0530

    Add files via upload

---
+++ week 4 06-03/4.prime50nd100.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+is_prime() {
+    num=$1
+    if [ $num -le 1 ]; then
+        return 1  
+    fi
+    
+    for (( i=2; i*i<=num; i++ ))
+    do
+        if [ $((num % i)) -eq 0 ]; then
+            return 1 
+        fi
+    done
+    return 0 
+}
+
+echo "Prime numbers between 50 and 100:"
+
+for (( num=50; num<=100; num++ ))
+do
+    if is_prime $num; then
+        echo $num
+    fi
+done
+
